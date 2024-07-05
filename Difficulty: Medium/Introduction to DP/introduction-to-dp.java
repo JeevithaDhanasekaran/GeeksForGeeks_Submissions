764d0c815936770e29e98e477ba9a461bcdf59c3
Jeevitha D <111889748+JeevithaDhanasekaran@users.noreply.github.com>
2024-07-05 10:27:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Introduction to DP/introduction-to-dp.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            Solution obj = new Solution();
+            long topDownans = obj.topDown(n);
+            long bottomUpans = obj.bottomUp(n);
+            if (topDownans != bottomUpans)
+                System.out.println(-1);
+            else
+                System.out.println(topDownans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static final int mod = 1000000007;
+
+    static long topDown(int n) {
+        // code here
+        long[] dp=new long[n+1];
+        return fibo(n,dp);
+    }
+    
+    static long fibo(int n,long dp[]){
+        if(n==0 || n==1)
+            return n;
+        if(dp[n] != 0)
+            return dp[n];
+        dp[n]=(fibo(n-1,dp)+fibo(n-2,dp))%mod;
+        return dp[n];
+    }
+
+    static long bottomUp(int n){
+        // code here
+        if(n==0 || n==1)
+            return n;
+        long[] dp=new long[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for (int i=2;i<=n;i++)
+            dp[i]=(dp[i-1]+dp[i-2]) %mod;
+        return dp[n];
+    }
+}
