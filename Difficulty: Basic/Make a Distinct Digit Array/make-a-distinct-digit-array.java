86deab53b421c1b1555bb02df38cdeaed5bb5c5e
Jeevitha D <111889748+JeevithaDhanasekaran@users.noreply.github.com>
2024-07-05 11:40:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Make a Distinct Digit Array/make-a-distinct-digit-array.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T-->0)
+        {
+            int n = sc.nextInt();
+            int[] nums = new int[n];
+            for(int i = 0; i < n; i++)
+                nums[i] = sc.nextInt();
+            Solution ob = new Solution();
+            int[] ans = ob.common_digits(nums);
+            for(int i = 0; i < ans.length; i++)
+                System.out.print(ans[i] + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int[] common_digits(int[] nums)
+    {
+        // code here
+        Set<Integer> set=new HashSet<>();
+        for(int num:nums){
+            while(num!=0){
+                set.add(num%10);
+                num/=10;
+            }
+        }
+        int i=0;
+        int[] s=new  int[set.size()];
+        for(int ele:set){
+            s[i++]=ele;
+        }
+        return s;
+    }
+}
