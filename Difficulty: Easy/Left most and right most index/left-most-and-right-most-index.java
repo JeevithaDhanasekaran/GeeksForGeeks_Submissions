0cad0d0d5e5c841e66b468a1c515f4e8b193c653
Jeevitha D <111889748+JeevithaDhanasekaran@users.noreply.github.com>
2024-07-05 18:46:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Left most and right most index/left-most-and-right-most-index.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class pair  {  
+    long first, second;  
+    public pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+}
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            long k = Long.parseLong(br.readLine().trim());
+            
+            Solution obj = new Solution();
+            pair ans = obj.indexes(a, k);
+            if (ans.first == -1 && ans.second == -1)
+                System.out.println(-1);
+            else
+                System.out.println(ans.first+" "+ans.second);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class pair  {  
+    long first, second;  
+    public pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+}
+*/
+
+class Solution {
+    
+    public pair indexes(long v[], long x)
+    {
+        pair p=new pair(-1,-1);
+        int left,right;
+        left=0;right=v.length-1;
+        for(int i=0;i<v.length;i++){
+            if(v[left]==x && p.first==-1){
+                p.first=left;
+            }
+            if(v[right]==x && p.second==-1){
+                p.second=right;
+            }
+            left++;
+            right--;
+        }
+        return p;
+        
+    }
+}
