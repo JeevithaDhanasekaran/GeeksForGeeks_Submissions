c06c9697937c86654e193a285306ab8f7e23608b
Jeevitha D <111889748+JeevithaDhanasekaran@users.noreply.github.com>
2024-07-05 15:36:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Triplet Sum in Array/triplet-sum-in-array.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int X = Integer.parseInt(inputLine[1]);
+            int A[] = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                A[i] = Integer.parseInt(inputLine[i]);
+            }
+            Solution ob = new Solution();
+            boolean ans = ob.find3Numbers(A, n, X);
+            System.out.println(ans ? 1 : 0);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Should return true if there is a triplet with sum equal
+    // to x in arr[], otherwise false
+    public static boolean find3Numbers(int arr[], int n, int x) {
+
+        Arrays.sort(arr);
+        int sum=0;
+        for(int i=0;i<n-1;i++){
+            int left=i+1;
+            int right=n-1;
+            while(left<right){
+                sum=arr[i]+arr[left]+arr[right];
+                if(sum==x)
+                    return true;
+                if(sum<x)
+                    left++;
+                if(sum>x)
+                    right--;
+            }
+        }
+        return false;
+    }
+}
